948587671ddaf7c4a5d3381762738339662fa482
doeltz <daniel.oeltz@scai.fraunhofer.de>
2023-04-28 17:18:48 +0200

    admin

--- rivapy/models/lucia_schwartz.py
+++ rivapy/models/lucia_schwartz.py
@@ -1,4 +1,4 @@
-from typing import Union, Callable
+from typing import Union, Callable, Tuple, List
 import numpy as np
 from rivapy.tools.interfaces import FactoryObject
 from rivapy.models.ornstein_uhlenbeck import OrnsteinUhlenbeck
@@ -59,8 +59,13 @@ class LuciaSchwartz(FactoryObject):
         return {'kappa': self.X1.speed_of_mean_reversion, 'sigma1': self.X1.volatility,
                 'mu': self.mu, 'sigma2': self.sigma2, 'f': self.f}
 
-    def _set_timegrid(self, timegrid):
-        
+    def _set_timegrid(self, timegrid: np.ndarray):
+        """
+        Sets the timegrid for simulation.
+
+        Args:
+            timegrid (np.ndarray): Timegrid for simulation.
+        """
         self._mu_grid = LuciaSchwartz._eval_grid(self.mu, timegrid)
         self._sigma2_grid = LuciaSchwartz._eval_grid(self.sigma2, timegrid)
         self._f_grid = LuciaSchwartz._eval_grid(self.f, timegrid)
@@ -69,17 +74,48 @@ class LuciaSchwartz(FactoryObject):
         return (n_timepoints-1, n_sims, 2)
 
 
-    def simulate(self, timegrid, start_value, rnd):
-        """_summary_
+    def compute_expected_value(self, x0: np.ndarray, T: Union[float, np.ndarray])->Union[float, np.ndarray]:
+        """
+        Computes the expected value of the model.
 
         Args:
-            timegrid (_type_): _description_
-            start_value (_type_): _description_
-            rnd (_type_): _description_
+            x0 (Union[float, np.ndarray]): Start value, either onedimensional (containing just the start value for X0 or X1) or twodimensional (different start values for X0 and X1 from e.g. a MC simulation).
+            T (Union[float, np.ndarray]): Terminal time.
 
         Returns:
-            _type_: _description_
+            Union[float, np.ndarray]: Expected value.
         """
+        if not isinstance(self.mu, float):
+            raise NotImplementedError("Only implemented for fixed value of mu.")
+        if not isinstance(self.sigma2, float):
+            raise NotImplementedError("Only implemented for fixed value of sigma2.")
+        if len(x0.shape)==1:
+            return self.X1.compute_expected_value(x0[0], T) + x0[1]+self.mu*T
+        return self.X1.compute_expected_value(x0[:,0], T) + x0[:,1]+self.mu*T
+
+    def _compute_fwd_value(self, forward: Tuple[float,float], timegrid: np.ndarray)->float:
+        tmp = np.where(timegrid>=forward[0])
+        if tmp.shape[0]==0:
+            raise ValueError("Forward start time not in timegrid.")
+        indices_start = np.where(timegrid>=forward[0])[0]
+        indices_end = np.where(timegrid<=forward[1])[0]
+        
+        
+
+    def simulate(self, timegrid, start_value, rnd, forwards:List[Tuple[float,float]]=None):
+        """
+        Simulates the model.
+
+        Args:
+            timegrid (np.ndarray): Timegrid for simulation.
+            start_value (Union[float, np.ndarray]): Start value for simulation.
+            rnd (np.ndarray): Random numbers for simulation.
+            forwards (np.ndarray, optional): Forwards for simulation. Defaults to None.
+        """
+        n_assets = 1
+        if forwards is not None:
+            n_assets = len(forwards)+1
+        result = np.full((timegrid.shape[0], rnd.shape[1], n_assets), np.NaN)
         self._set_timegrid(timegrid)
         rnd_ = np.copy(rnd)
         rnd_[:,:,1] = self.rho*rnd[:,:,0] + np.sqrt(1.0-self.rho**2)*rnd[:,:,1]
@@ -93,6 +129,6 @@ class LuciaSchwartz(FactoryObject):
         X1 = self.X1.simulate(timegrid, start_value=start_X1, rnd=rnd_[:,:,0])
        
         for i in range(timegrid.shape[0]-1):
-            X2[i+1,:] = X2[i,:]*self.X1._delta_t[i] + self._sigma2_grid[i]*rnd[i,:,1]*self.X1._sqrt_delta_t[i]
-        return  X1 + X2  + self._f_grid[:,np.newaxis]
+            X2[i+1,:] = X2[i,:] + self._mu_grid[i]*self.X1._delta_t[i] + self._sigma2_grid[i]*rnd[i,:,1]*self.X1._sqrt_delta_t[i]
+        return  X1 #+ X2  + self._f_grid[:,np.newaxis]
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -228,5 +228,27 @@ class OrnsteinUhlenbeckTest(unittest.TestCase):
 		self.assertAlmostEqual(0.0, ou_model.mean_reversion_level, places=2)
 		self.assertAlmostEqual(5.0, ou_model.speed_of_mean_reversion, delta=0.5)
 
+	def test_expectation(self):
+		"""Simple test for analytical expectation: Compare analytical expectation with MC expectation
+		"""
+		np.random.seed(42)
+		timegrid = np.linspace(0.0,1.0,365) # simulate on daily timegrid over 1 yr horizon
+		ou_model = models.OrnsteinUhlenbeck(speed_of_mean_reversion = 5.0, volatility=0.1)
+		n_sims = 1000
+		sim = ou_model.simulate(timegrid, start_value=0.2,rnd=np.random.normal(size=(timegrid.shape[0],n_sims)))
+		self.assertAlmostEqual(sim[-1,:].mean(), ou_model.compute_expected_value(0.2,1.0), places=3)
+		
+class LuciaSchwartzTest(unittest.TestCase):
+	def test_expectation(self):
+		"""Simple test for analytical expectation: Compare analytical expectation with MC expectation
+		"""
+		np.random.seed(42)
+		timegrid = np.linspace(0.0,1.0,800) # simulate on daily timegrid over 1 yr horizon
+		ls_model = models.LuciaSchwartz(rho=-0.9, kappa=1.0, sigma1=0.5, mu=0.15, sigma2=0.3)
+		n_sims = 160_000
+		sim = ls_model.simulate(timegrid, start_value=np.array([0.0,0.0]),rnd=np.random.normal(size=ls_model.rnd_shape(timegrid.shape[0],n_sims)))
+		self.assertAlmostEqual(sim[-1,:].mean(), ls_model.compute_expected_value(x0=np.array([0.0,0.0]), T=1.0), places=2)
+
+
 if __name__ == '__main__':
     unittest.main()
